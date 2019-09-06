6877532735696589ea37870753c4a9434a69231d
CromulonB <blissj@gmail.com>
2019-09-06 09:50:50 -0400

    Firmware Update 2

--- Software/RestoreSingle.sh
+++ Software/RestoreSingle.sh
@@ -1,1 +1,1 @@
-sudo  python ~/.local/lib/python2.7/site-packages/esptool.py --port /dev/ttyUSB0 --baud 921600 write_flash 0x00000 MultiPassFix1.img
+sudo  python ~/.local/lib/python2.7/site-packages/esptool.py --port /dev/ttyUSB0 --baud 921600 write_flash 0x00000 MultiPassFix2.img
